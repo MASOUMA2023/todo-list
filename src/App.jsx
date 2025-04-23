8bb676eacd2e7171e35c98d03b2173a06ce26058
MASOUMA2023 <ahmadimasooma12@gmail.com>
2025-04-23 13:13:38 -0400

    controlled components and forms

--- src/App.jsx
+++ src/App.jsx
@@ -6,23 +6,28 @@ import TodoForm from './TodoForm';
 
 function App() {
   const [todoList, setTodoList] = useState([]);
-
-  const addTodo = (title) => {
+ const addTodo = (title) => {
     const newTodo = {
       id: Date.now(),
-      title: title
+      title: title,
+      isCompleted : false
     };
     setTodoList([...todoList, newTodo]);
+  }
+    const completeTodo = (id) =>{
+      const updatedTodos = todoList.map((todo)=>
+        todo.id === id ? { ...todo, isCompleted: true} : todo);
+        setTodoList (updatedTodos);
   };
   return (
     <>
     <div className="App">
       <h1>My Todo App</h1>
       <TodoForm onAddTodo={addTodo} />
-      <TodoList todoList={todoList} />
+      <TodoList todoList={todoList} onCompleteTodo={completeTodo} />
     </div>
     </>
   )
 }
 
-export default App
+export default App;
